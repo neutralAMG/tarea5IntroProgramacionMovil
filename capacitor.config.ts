7438a7fe117253aa22529c3d51fe39d023546608
neutralAMG <amoscoso698@gmail.com>
2025-02-27 10:21:21 -0400

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Tarea5IntroProgramacionMovil',
+  webDir: 'www'
+};
+
+export default config;
