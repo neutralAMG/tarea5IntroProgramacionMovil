4ad55b8ce91dafbbcf8e60ee2e42db9d751ef41c
neutralAMG <amoscoso698@gmail.com>
2025-02-28 16:15:54 -0400

    last commit with the dependecies to build the android apk

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,12 @@ import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 import { IonApp, IonSplitPane, IonMenu, IonContent, IonList, IonListHeader, IonNote, IonMenuToggle, IonItem, IonIcon, IonLabel, IonRouterOutlet, IonRouterLink } from '@ionic/angular/standalone';
 import { addIcons } from 'ionicons';
-import { mailOutline, mailSharp, paperPlaneOutline, paperPlaneSharp, heartOutline, heartSharp, archiveOutline, archiveSharp, trashOutline, trashSharp, warningOutline, warningSharp, bookmarkOutline, bookmarkSharp } from 'ionicons/icons';
+import { mailOutline, mailSharp, paperPlaneOutline, paperPlaneSharp, heartOutline, heartSharp, archiveOutline, 
+  archiveSharp, trashOutline, trashSharp, warningOutline, warningSharp, bookmarkOutline, bookmarkSharp, 
+  homeSharp, homeOutline , bookSharp, bookOutline ,bodySharp , bodyOutline , keySharp, keyOutline , schoolSharp, schoolOutline  } from 'ionicons/icons';
+import { StatusBar } from '@capacitor/status-bar';
 
+StatusBar.setOverlaysWebView({overlay: false})
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
@@ -13,14 +17,16 @@ import { mailOutline, mailSharp, paperPlaneOutline, paperPlaneSharp, heartOutlin
 })
 export class AppComponent {
   public appPages = [
-    { title: 'Home', url: '/folder/inbox', icon: 'mail' },
-    { title: 'About', url: '/about', icon: 'trash' },
-    { title: 'Characters', url: '/characters', icon: 'paper-plane' },
+    { title: 'Home', url: '/folder/inbox', icon: 'home' },
+    { title: 'About', url: '/about', icon: 'book' },
+    { title: 'Characters', url: '/characters', icon: 'body' },
     { title: 'Moments', url: '/moments', icon: 'heart' },
-    { title: 'My Experience', url: '/my-experience', icon: 'archive' },
-    { title: 'Hire Me', url: '/hire-me', icon: 'warning' },
+    { title: 'My Experience', url: '/my-experience', icon: 'key' },
+    { title: 'Hire Me', url: '/hire-me', icon: 'school' },
   ];
   constructor() {
-    addIcons({ mailOutline, mailSharp, paperPlaneOutline, paperPlaneSharp, heartOutline, heartSharp, archiveOutline, archiveSharp, trashOutline, trashSharp, warningOutline, warningSharp, bookmarkOutline, bookmarkSharp });
+    addIcons({ mailOutline, mailSharp, paperPlaneOutline, paperPlaneSharp, heartOutline, heartSharp, archiveOutline, archiveSharp, trashOutline, 
+      trashSharp, warningOutline, warningSharp, bookmarkOutline, bookmarkSharp , homeSharp, homeOutline ,  bookSharp, bookOutline , 
+      bodySharp , bodyOutline , keySharp, keyOutline , schoolSharp, schoolOutline  });
   }
 }
